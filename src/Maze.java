b87cc720a2dfd3407f8a67890859aa9908f7a393
Ramsey Yaghi <ramsey@reindeere.ca>
2026-03-24 08:19:23 -0400

    WIP

--- src/Maze.java
+++ src/Maze.java
@@ -1,14 +1,19 @@
 public class Maze {
-    
+
+    /**
+     * Enum for directions to improve readability.
+     */
+    public static final enum Dir {
+        NORTH(0), EAST(1), SOUTH(2), WEST(3);
+        private final int idx;
+        Dir(int idx) { this.idx = idx; }
+        public int idx() { return idx; }
+    }
     /**
      * Represents a single cell in the maze grid.
      * Null pointer in neighbors array indicates a wall or boundary.
      * 
      */
-
-     public static enum Direction {
-            NORTH, EAST, SOUTH, WEST
-        };
 
     public static class Node {
         public int row;
@@ -26,10 +31,24 @@ public class Maze {
             this.isGoal = false;
             this.neighbors = new Node[4]; // [N, E, S, W]
         }
+
+        public Node(int row, int col, boolean isGoal) {
+            this.row = row;
+            this.col = col;
+            this.isGoal = isGoal;
+            this.neighbors = new Node[4]; // [N, E, S, W]
+        }
+
+        public Node getNeighbor(Dir d) { return neighbors[d.idx()]; }
+        public void setNeighbor(Dir d, Node n) { neighbors[d.idx()] = n; }
+
     }
     
+
+    // Maze grid and dimensions
     private Node[][] grid;
-    private int height;
+    private boolean[][] occupied;
+    private int length;
     private int width;
     private Node start;
     private Node goal;
@@ -38,10 +57,11 @@ public class Maze {
      * Initialize maze with given dimensions.
      * All cells start as non-wall paths.
      */
-    public Maze(int height, int width) {
-        this.height = height;
+    public Maze(int length, int width) {
+        this.length = length;
         this.width = width;
-        this.grid = new Node[height][width];
+        this.grid = new Node[length][width];
+        this.occupied = new boolean[length][width];
         initializeGrid();
     }
     
@@ -50,69 +70,65 @@ public class Maze {
      */
     private void initializeGrid() {
         // Create all nodes
-        for (int r = 0; r < height; r++) {
-            for (int c = 0; c < width; c++) {
-                grid[r][c] = new Node(r, c);
+        for (int l = 0; l < length; l++) {
+            for (int w = 0; w < width; w++) {
+                grid[l][w] = new Node(l, w);
+                occupied[l][w] = false; // Initially, all cells are unoccupied, no paths
             }
         }
         
-        // Link neighbors [N, E, S, W]
-        for (int r = 0; r < height; r++) {
-            for (int c = 0; c < width; c++) {
-                Node current = grid[r][c];
-                
-                // NORTH
-                if (r > 0) current.neighbors[Direction.NORTH.ordinal()] = grid[r - 1][c];
-                // EAST
-                if (c < width - 1) current.neighbors[Direction.EAST.ordinal()] = grid[r][c + 1];
-                // SOUTH
-                if (r < height - 1) current.neighbors[Direction.SOUTH.ordinal()] = grid[r + 1][c];
-                // WEST
-                if (c > 0) current.neighbors[Direction.WEST.ordinal()] = grid[r][c - 1];
-            }
-        }
     }
     
+
     /**
-     * Create a wall between two nodes by setting their respective pointers to null.
+     * Create a wall between two adjacent nodes.
+     * This removes the neighbor reference in both nodes.
      */
     public void createWall(int row1, int col1, int row2, int col2) {
-        if (!isInBounds(row1, col1) || !isInBounds(row2, col2)) {
-            return;
-        }
-        
-        Node node1 = grid[row1][col1];
-        Node node2 = grid[row2][col2];
-        
-        // Determine direction from node1 to node2 and vice versa
-        int dRow = row2 - row1;
-        int dCol = col2 - col1;
-        
-        // Only allow adjacent cells
-        if (Math.abs(dRow) + Math.abs(dCol) != 1) {
-            return;
-        }
-        
-        if (dRow == -1) { // node2 is NORTH of node1
-            node1.neighbors[Direction.NORTH.ordinal()] = null;
-            node2.neighbors[Direction.SOUTH.ordinal()] = null;
-        } else if (dRow == 1) { // node2 is SOUTH of node1
-            node1.neighbors[Direction.SOUTH.ordinal()] = null;
-            node2.neighbors[Direction.NORTH.ordinal()] = null;
-        } else if (dCol == -1) { // node2 is WEST of node1
-            node1.neighbors[Direction.WEST.ordinal()] = null;
-            node2.neighbors[Direction.EAST.ordinal()] = null;
-        } else if (dCol == 1) { // node2 is EAST of node1
-            node1.neighbors[Direction.EAST.ordinal()] = null;
-            node2.neighbors[Direction.WEST.ordinal()] = null;
+        if (isInBounds(row1, col1) && isInBounds(row2, col2)) {
+            Node node1 = grid[row1][col1];
+            Node node2 = grid[row2][col2];
+            
+            // Determine direction from node1 to node2
+            if (row1 == row2) {
+                // Same row: EAST or WEST
+                if (col1 < col2) {
+                    // node1 is WEST of node2
+                    node1.neighbors[Maze.Dir.EAST.ordinal()] = null;
+                    node2.neighbors[Maze.Dir.WEST.ordinal()] = null;
+                } else {
+                    // node1 is EAST of node2
+                    node1.neighbors[Maze.Dir.WEST.ordinal()] = null;
+                    node2.neighbors[Maze.Dir.EAST.ordinal()] = null;
+                }
+            } else if (col1 == col2) {
+                // Same column: NORTH or SOUTH
+                if (row1 < row2) {
+                    // node1 is NORTH of node2
+                    node1.neighbors[Maze.Dir.SOUTH.ordinal()] = null;
+                    node2.neighbors[Maze.Dir.NORTH.ordinal()] = null;
+                } else {
+                    // node1 is SOUTH of node2
+                    node1.neighbors[Maze.Dir.NORTH.ordinal()] = null;
+                    node2.neighbors[Maze.Dir.SOUTH.ordinal()] = null;
+                }
+            }
         }
     }
-    
+
+    public void createCell(int row, int col, int direction) {
+        if (isInBounds(row, col)) {
+            occupied[row][col] = true;
+            grid[row][col] = new Node(row, col);
+        }
+    }   
+
+
     /**
      * Set the start position.
      */
     public void setStart(int row, int col) {
-        if (isInBounds(row, col)) {
+        if (isOccupied(row, col)) {
             this.start = grid[row][col];
         }
     }
@@ -121,7 +137,7 @@ public class Maze {
      * Set the goal position.
      */
     public void setGoal(int row, int col) {
-        if (isInBounds(row, col)) {
+        if (isOccupied(row, col)) {
             this.goal = grid[row][col];
             this.goal.isGoal = true;
         }
@@ -145,7 +161,7 @@ public class Maze {
      * Get a specific node by coordinates.
      */
     public Node getNode(int row, int col) {
-        if (isInBounds(row, col)) {
+        if (isOccupied(row, col)) {
             return grid[row][col];
         }
         return null;
@@ -154,12 +170,19 @@ public class Maze {
     /**
      * Check if coordinates are within maze bounds.
      */
-    private boolean isInBounds(int row, int col) {
-        return row >= 0 && row < height && col >= 0 && col < width;
+    public boolean isInBounds(int row, int col) {
+        return row >= 0 && row < length && col >= 0 && col < width;
+    }
+
+    public boolean isOccupied(int row, int col) {
+        if (isInBounds(row, col)) {
+            return occupied[row][col];
+        }
+        return false;
     }
     
-    public int getHeight() {
-        return height;
+    public int getLength() {
+        return length;
     }
     
     public int getWidth() {
--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,48 +1,6 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class MazeSolver {
-    
-    private Maze maze;
-    
-    public MazeSolver(Maze maze) {
-        this.maze = maze;
-    }
-    
-    /**
-     * Get valid neighbors for a given node (non-null neighbors only).
-     * Null pointers represent walls or boundaries.
-     * Useful for BFS, DFS, A* algorithms.
-     */
-    public List<Maze.Node> getValidNeighbors(Maze.Node node) {
-        List<Maze.Node> neighbors = new ArrayList<>();
-        
-        // Check all four directions using Direction enum
-        for (Maze.Node.Direction direction : Maze.Node.Direction.values()) {
-            Maze.Node neighbor = node.neighbors[direction.ordinal()];
-            if (neighbor != null) {
-                neighbors.add(neighbor);
-            }
-        }
-        
-        return neighbors;
-    }
-    
-    // TODO: Implement BFS algorithm
-    public List<Maze.Node> solveBFS() {
-        return null;
-    }
-    
-    // TODO: Implement DFS algorithm
-    public List<Maze.Node> solveDFS() {
-        return null;
-    }
-    
-    // TODO: Implement A* algorithm
-    public List<Maze.Node> solveAStar() {
-        return null;
-    }
     
     public static void main(String[] args) {
         // Example usage:
@@ -54,4 +12,3 @@ public class MazeSolver {
         // MazeSolver solver = new MazeSolver(maze);
         // List<Maze.Node> path = solver.solveBFS();
     }
-}
